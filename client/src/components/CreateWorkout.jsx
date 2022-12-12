0a919d6875678276d024cdb33b41be82d01bbcc3
Jerry Chen <jerry.chen33193@gmail.com>
2022-12-12 10:24:50 -0500

    add workout

---
+++ client/src/components/CreateWorkout.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
+
+const CreateWorkoutForm = (props) => {
+  const [formState, setFormState] = useState({
+    exercise: '',
+    numberOfSets: '',
+    numberOfReps: ''
+  })
+
+  let navigate = useNavigate()
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    let newWorkout = await axios.post(`${BASE_URL}/add/work-out`, formState)
+
+
+export default CreateRideForm
