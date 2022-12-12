f8c2371357e0cb4cc301244e00640037245f6c72
Jerry Chen <jerry.chen33193@gmail.com>
2022-12-12 16:20:26 -0500

    workoutCard

--- client/src/components/WorkCard.jsx
+++ client/src/components/WorkCard.jsx
@@ -3,7 +3,27 @@ import { Link, useParams } from 'react-router-dom'
 import axios from 'axios'
 
 const WorkCard = (props) => {
-  
+   return (
+    <div>
+      <h2>{props.workout.exercise}</h2>
+
+        <div className="info">
+          <p className="desc">{props.workout.numberOfSets}</p>
+          <div>
+            <Link className="btn" to={`/work-out/${props.workout._id}`}>
+              View
+            </Link>
+
+            {/* <button
+              className="btn danger"
+              onClick={() => props.deletePark(props.park._id)}
+            >
+              Delete
+            </button> */}
+          </div>
+        </div>
+      </div>
+  )
 }
 
 export default WorkCard
