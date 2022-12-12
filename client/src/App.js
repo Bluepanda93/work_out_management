204a9312aff25834f71112fb204181e0457904c9
Jerry Chen <jerry.chen33193@gmail.com>
2022-12-12 10:37:51 -0500

    workout form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
+import CreateWorkoutForm from './components/CreateWorkoutForm'
 import './App.css'
 
 function App() {
+  const resetScrollPos = () => {
+    window.scrollTo(0, 0)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +17,10 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route
+            path="/add/work-out"
+            element={<CreateWorkoutForm resetScrollPos={resetScrollPos} />}
+          />
         </Routes>
       </main>
     </div>
---
+++ client/src/components/CreateWorkoutForm.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
+
+const CreateWorkoutForm = (props) => {
+  const [formState, setFormState] = useState({
+    exercise: '',
+    numberOfSets: '',
+    numberOfReps: ''
+  })
+
+  let navigate = useNavigate()
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    let newWorkout = await axios.post(`${BASE_URL}/add/work-out`, formState)
+
+    setFormState({
+      exercise: '',
+      numberOfSets: '',
+      numberOfReps: ''
+    })
+
+    navigate(`/work-out/${newWorkout.data._id}`)
+  }
+
+  useEffect(() => {
+    props.resetScrollPos()
+  }, [])
+
+  return (
+    <div className="create-workout">
+      <h1>Submit A Workout Here!</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="exercise">Exercise Type: </label>
+        <input
+          id="exercise"
+          value={formState.exercise}
+          onChange={handleChange}
+          placeholder="Name"
+          required
+        />
+
+        <label htmlFor="numberOfSets">Number Of Sets: </label>
+        <input
+          type="number"
+          id="numberOfSets"
+          value={formState.numberOfSets}
+          onChange={handleChange}
+          placeholder="numberOfSets"
+          required
+        />
+
+        <label htmlFor="numberOfReps">Number Of Reps: </label>
+        <input
+          type="number"
+          id="numberOfReps"
+          value={formState.numberOfReps}
+          onChange={handleChange}
+          placeholder="numberOfReps"
+          required
+        />
+
+        <button className="btn" type="submit">
+          Submit New Workout!
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateWorkoutForm
