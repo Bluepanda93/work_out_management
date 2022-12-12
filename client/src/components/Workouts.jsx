c3052e5e93fb44e6e4971c4f72cb0331aeae4882
Jerry Chen <jerry.chen33193@gmail.com>
2022-12-12 13:14:48 -0500

    workouts component

---
+++ client/src/components/Workouts.jsx
@@ -0,0 +1,10 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import WorkCard from './WorkCard'
+import { BASE_URL } from '../globals'
+
+const Workouts = (props) => {
+
+}
+
+export default Workouts
