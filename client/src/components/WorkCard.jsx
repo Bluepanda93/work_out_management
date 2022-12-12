7a7a0205db6641a59157eb85e259fadde89a66f4
Jerry Chen <jerry.chen33193@gmail.com>
2022-12-12 13:15:46 -0500

    WorkCard component

---
+++ client/src/components/WorkCard.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import axios from 'axios'
+
+const WorkCard = (props) => {
+  
+}
+
+export default WorkCard
