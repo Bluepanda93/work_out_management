ed398af32ee2d225e3eaec99e6f0df7cd7bfd4dc
Jerry Chen <jerry.chen33193@gmail.com>
2022-12-13 15:52:01 -0500

    delete working

--- client/src/components/WorkCard.jsx
+++ client/src/components/WorkCard.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 const WorkCard = () => {
   const [work, setWork] = useState([])
+  const [form, setForm] = useState({
+    exercise: "",
+    numberOfSets: "",
+    numberOfReps: ""
+  })
   let {id} = useParams()
 
+  
   useEffect(()=>{
     const apiCall = async () => {
       let response = await axios.get(`http://localhost:3001/work-out/${id}`)
@@ -13,6 +20,23 @@ const WorkCard = () => {
     }
     apiCall()
   }, [])
+  
+  const handleChange = (event) => {
+    setForm({...form, [event.target.id]: event.target.value})
+  }
+
+  const deleteWorkout = async () => {
+    let deleteCard = await axios.delete(`http://localhost:3001/work-out/${id}`)
+    setWork([work, deleteCard.data])
+    setForm({
+      exercise: "",
+      numberOfSets: "",
+      numberOfReps: ""
+    })
+  }
+
+  let navigate = useNavigate()
+
   return (
     <div>
       <section>
@@ -20,21 +44,10 @@ const WorkCard = () => {
           {work.exercise}
         </h1>
         <h1>{work.numberOfSets}</h1>
+        <h1>{work.numberOfReps}</h1>
       </section>
-      {/* <h2>{workout?.exercise}</h2>
-
-        <div className="info">
-          <p className="desc">{workout?.numberOfSets}</p>
-          <div>
-
-            <button
-              className="btn danger"
-              onClick={() => .deletePark(.park._id)}
-            >
-              Delete
-            </button>
-          </div>
-        </div> */}
+      <button onClick={deleteWorkout}>Delete</button>
+      <button onClick={() => navigate('/work-outs')}>Back</button>
       </div>
   )
 }
--- client/src/components/Workouts.jsx
+++ client/src/components/Workouts.jsx
@@ -6,12 +6,6 @@ import { BASE_URL } from '../globals'
 
 const Workouts = () => {
   const [workouts, setWorkouts] = useState([])
-  const [deleteAWorkout, setDeletedWorkout] = useState(false)
-
-  const deleteWorkout = async (id) => {
-    let deleteCard = await axios.delete(`${BASE_URL}/work-out/${id}`)
-    setDeletedWorkout(!deleteAWorkout)
-  }
 
   useEffect(() => {
     const getWorkouts = async () => {
@@ -22,7 +16,7 @@ const Workouts = () => {
 
     // props.resetScrollPos()
     getWorkouts()
-  }, [deleteAWorkout])
+  }, [])
 
   let navigate = useNavigate()
 
@@ -38,7 +32,6 @@ const Workouts = () => {
         <div
           key={work.id}
           className="bigspace">
-          {/* <WorkCard deleteWork={deleteWorkout} /> */}
           <h1>{work.exercise}</h1>
           <h1>{work.numberOfSets}</h1>
           <h1>{work.numberOfReps}</h1>
