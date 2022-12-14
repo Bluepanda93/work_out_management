44e0925b659dac6c0c2552a0a5684225d8a50deb
Jerry Chen <jerry.chen33193@gmail.com>
2022-12-14 16:37:55 -0500

    fonts

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,13 +7,13 @@ const Home = () => {
   return (
   <div className="home">
   <div className="home-header">
-    <h1>You dont have to get ready, when you stay ready!</h1>
+    <h1 className="home1">You dont have to get ready, when you stay ready!</h1>
     <div>
       <img className="logo"
       src="https://i.ibb.co/Jdy87cT/Logo-Makr-59-BLre.png" alt="Logo-Makr-59-BLre" border="0"/>
       </div>
-    <h3>Save Time!</h3>
-    <h3>Get the work done first!</h3>
+    <h3 className="home2">Save Time!</h3>
+    <h3 className="home3">Get the work done first!</h3>
   </div>
   </div>
   )
