4bcc912fdfead181d4e6a1e9a48b669546d99f79
Jerry Chen <jerry.chen33193@gmail.com>
2022-12-13 22:40:44 -0500

    user component

--- client/src/components/CreateUserForm.jsx
+++ client/src/components/CreateUserForm.jsx
@@ -1,63 +1,63 @@
-// import { useState, useEffect } from 'react'
-// import { useNavigate } from 'react-router-dom'
-// import axios from 'axios'
-// import { BASE_URL } from '../globals'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
 
-// const CreateUserForm = (props) => {
-//   const [formState, setFormState] = useState({
-//     firstName: '',
-//     lastName: ''
-//   })
+const CreateUserForm = (props) => {
+  const [formState, setFormState] = useState({
+    firstName: '',
+    lastName: ''
+  })
 
-//   let navigate = useNavigate()
+  let navigate = useNavigate()
 
-//   const handleChange = (event) => {
-//     setFormState({ ...formState, [event.target.id]: event.target.value })
-//   }
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
 
-//   const handleSubmit = async (event) => {
-//     event.preventDefault()
-//     let newUser = await axios.post(`${BASE_URL}/add/user`, formState)
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    let newUser = await axios.post(`${BASE_URL}/add/user`, formState)
 
-//     setFormState({
-//       firstName: '',
-//       lastName: ''
-//     })
+    setFormState({
+      firstName: '',
+      lastName: ''
+    })
 
-//     navigate(`/user/${newUser.data._id}`)
-//   }
+    navigate(`/user/${newUser.data._id}`)
+  }
 
-//   useEffect(() => {
-//     props.resetScrollPos()
-//   }, [])
+  useEffect(() => {
+    props.resetScrollPos()
+  }, [])
 
-//   return (
-//     <div className="create-user">
-//       <h1>Submit A User Here!</h1>
-//       <form onSubmit={handleSubmit}>
-//         <label htmlFor="firstName">First Name: </label>
-//         <input
-//           id="firstName"
-//           value={formState.firstName}
-//           onChange={handleChange}
-//           placeholder="First Name"
-//           required
-//         />
+  return (
+    <div className="create-user">
+      <h1>Submit A User Here!</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name: </label>
+        <input
+          id="firstName"
+          value={formState.firstName}
+          onChange={handleChange}
+          placeholder="First Name"
+          required
+        />
 
-//         <label htmlFor="lastName">Last Name: </label>
-//         <input
-//           id="lastName"
-//           value={formState.lastName}
-//           onChange={handleChange}
-//           placeholder="Last Name"
-//           required
-//         />
-//         <button className="btn" type="submit">
-//           Submit New User!
-//         </button>
-//       </form>
-//     </div>
-//   )
-// }
+        <label htmlFor="lastName">Last Name: </label>
+        <input
+          id="lastName"
+          value={formState.lastName}
+          onChange={handleChange}
+          placeholder="Last Name"
+          required
+        />
+        <button className="btn" type="submit">
+          Submit New User!
+        </button>
+      </form>
+    </div>
+  )
+}
 
-// export default CreateUserForm
+export default CreateUserForm
--- client/src/components/user.jsx
+++ client/src/components/user.jsx
@@ -1,22 +1,39 @@
-// import axios from 'axios'
-// import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 // import { useNavigate } from 'react-router-dom'
 // import WorkCard from './WorkCard'
-// import { BASE_URL } from '../globals'
+import { BASE_URL } from '../globals'
 
-// const Users = () => {
-//   const [workouts, setWorkouts] = useState([])
+const Users = () => {
+  const [users, setUser] = useState([])
 
-//   useEffect(() => {
-//     const getWorkouts = async () => {
-//       const res = await axios.get(`${BASE_URL}/work-outs`)
+  useEffect(() => {
+    const getUsers = async () => {
+      const res = await axios.get(`${BASE_URL}/users`)
 
-//       setWorkouts(res.data)
-//     }
+      setUser(res.data)
+    }
 
-//     // props.resetScrollPos()
-//     getWorkouts()
-//   }, [])
-// }
+    // props.resetScrollPos()
+    getUsers()
+  }, [])
 
-// export default Users
+  return (
+    <div>
+      <h1>All Users</h1>
+
+      {users.map((user) => (
+        <div
+          key={user.id}
+          className="bigspace">
+          <h1>{user.firstName}</h1>
+          <h1>{user.lastName}</h1>
+          {/* <h2 onClick={() => getWorkoutDetails(user._id)}>View Workout</h2> */}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+
+export default Users
